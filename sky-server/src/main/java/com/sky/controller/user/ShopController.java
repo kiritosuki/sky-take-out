99e24b2e79efad50fce0e7f411f8ce70b3778eb4
kiritosuki <3322640054@qq.com>
2025-05-17 16:46:30 +0800

    用户端-获取店铺营业状态

--- sky-server/src/main/java/com/sky/controller/user/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -1,4 +1,4 @@
-package com.sky.controller.admin;
+package com.sky.controller.user;
 
 import com.sky.result.Result;
 import com.sky.service.ShopService;
@@ -7,25 +7,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/admin/shop")
-@RestController("/adminShopController")
+@RequestMapping("/user/shop")
+@RestController("userShopController")
 public class ShopController {
     @Autowired
     private ShopService shopService;
 
-    private static final Logger log = LoggerFactory.getLogger(ShopController.class);
-
-    /**
-     * 设置店铺营业状态
-     * @param status
-     * @return
-     */
-    @PutMapping("/{status}")
-    public Result setShopStatus(@PathVariable("status") Integer status){
-        log.info("设置店铺营业状态为: {}", status == 1 ? "营业中" : "打烊中");
-        shopService.setShopStatus(status);
-        return Result.success();
-    }
+    private static final Logger log = LoggerFactory.getLogger(com.sky.controller.admin.ShopController.class);
 
     /**
      * 获取店铺营业状态
